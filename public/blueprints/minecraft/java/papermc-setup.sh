19a8daba3d5505daa9865d2c6210936957a4258c
mel <mcmelon@nodiumhosting.com>
2025-07-08 00:42:54 +0200

    Update papermc-setup.sh

--- public/blueprints/minecraft/java/papermc-setup.sh
+++ public/blueprints/minecraft/java/papermc-setup.sh
@@ -1,62 +1,96 @@
 #!/bin/ash
-# Software: PaperMC
-# Variables: MINECRAFT_VERSION?, BUILD_NUMBER?, SERVER_BINARY
 
-apk add --no-cache curl jq
+# Paper Installation Script
 
-PROJECT_DATA=$(curl -fsS https://api.papermc.io/v2/projects/paper)
-if [ -z "$PROJECT_DATA" ]; then
-  echo "Error: Failed to fetch data from PaperMC API." >&2
-  exit 1
-fi
+#
 
-_DESIRED_MINECRAFT_VERSION="${MINECRAFT_VERSION}"
-if [ -n "${_DESIRED_MINECRAFT_VERSION}" ] && \
-   echo "$PROJECT_DATA" | jq -e --arg VERSION "$_DESIRED_MINECRAFT_VERSION" '.versions | any(. == $VERSION)' > /dev/null 2>&1; then
-  echo "Using Minecraft version: ${_DESIRED_MINECRAFT_VERSION}."
+# Server Files: /data
+
+PROJECT=paper
+
+
+
+if [ -n "${DL_PATH}" ]; then
+
+	echo -e "Using supplied download url: ${DL_PATH}"
+
+	DOWNLOAD_URL=`eval echo $(echo ${DL_PATH} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
 else
-  _LATEST_VERSION=$(echo "$PROJECT_DATA" | jq -r '.versions[-1]')
-  if [ -n "${_DESIRED_MINECRAFT_VERSION}" ]; then
-    echo "Minecraft version '${_DESIRED_MINECRAFT_VERSION}' not found or invalid. Using latest: ${_LATEST_VERSION}."
-  else
-    echo "Minecraft version not specified. Using latest: ${_LATEST_VERSION}."
-  fi
-  MINECRAFT_VERSION="${_LATEST_VERSION}"
+
+	VER_EXISTS=`curl -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r --arg VERSION $MINECRAFT_VERSION '.versions[] | contains($VERSION)' | grep -m1 true`
+
+	LATEST_VERSION=`curl -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r '.versions' | jq -r '.[-1]'`
+
+
+
+	if [ "${VER_EXISTS}" == "true" ]; then
+
+		echo -e "Version is valid. Using version ${MINECRAFT_VERSION}"
+
+	else
+
+		echo -e "Specified version not found. Defaulting to the latest ${PROJECT} version"
+
+		MINECRAFT_VERSION=${LATEST_VERSION}
+
+	fi
+
+
+
+	BUILD_EXISTS=`curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION} | jq -r --arg BUILD ${BUILD_NUMBER} '.builds[] | tostring | contains($BUILD)' | grep -m1 true`
+
+	LATEST_BUILD=`curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION} | jq -r '.builds' | jq -r '.[-1]'`
+
+
+
+	if [ "${BUILD_EXISTS}" == "true" ]; then
+
+		echo -e "Build is valid for version ${MINECRAFT_VERSION}. Using build ${BUILD_NUMBER}"
+
+	else
+
+		echo -e "Using the latest ${PROJECT} build for version ${MINECRAFT_VERSION}"
+
+		BUILD_NUMBER=${LATEST_BUILD}
+
+	fi
+
+
+
+	JAR_NAME=${PROJECT}-${MINECRAFT_VERSION}-${BUILD_NUMBER}.jar
+
+
+
+	echo "Version being downloaded"
+
+	echo -e "MC Version: ${MINECRAFT_VERSION}"
+
+	echo -e "Build: ${BUILD_NUMBER}"
+
+	echo -e "JAR Name of Build: ${JAR_NAME}"
+
+	DOWNLOAD_URL=https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds/${BUILD_NUMBER}/downloads/${JAR_NAME}
+
 fi
 
-VERSION_BUILD_DATA=$(curl -fsS "https://api.papermc.io/v2/projects/paper/versions/${MINECRAFT_VERSION}")
-if [ -z "$VERSION_BUILD_DATA" ]; then
-  echo "Error: Failed to fetch build data for Minecraft version ${MINECRAFT_VERSION}." >&2
-  exit 1
-fi
 
-_LATEST_BUILD=$(echo "$VERSION_BUILD_DATA" | jq -r '.builds[-1]')
-_DESIRED_BUILD_NUMBER="${BUILD_NUMBER}"
 
-if [ -n "$_DESIRED_BUILD_NUMBER" ] && \
-   echo "$VERSION_BUILD_DATA" | jq -e --argjson BN "$_DESIRED_BUILD_NUMBER" '.builds | any(. == $BN)' > /dev/null 2>&1; then
-  echo "Using build number: ${_DESIRED_BUILD_NUMBER} for Minecraft ${MINECRAFT_VERSION}."
-  BUILD_NUMBER="${_DESIRED_BUILD_NUMBER}"
-else
-  if [ -n "$_DESIRED_BUILD_NUMBER" ]; then
-    echo "Build '${_DESIRED_BUILD_NUMBER}' not found or invalid for Minecraft ${MINECRAFT_VERSION}. Using latest build: ${_LATEST_BUILD}."
-  else
-    echo "Build number not specified for Minecraft ${MINECRAFT_VERSION}. Using latest build: ${_LATEST_BUILD}."
-  fi
-  BUILD_NUMBER="${_LATEST_BUILD}"
-fi
+cd /data
 
-JAR_NAME="paper-${MINECRAFT_VERSION}-${BUILD_NUMBER}.jar"
-DOWNLOAD_URL="https://api.papermc.io/v2/projects/paper/versions/${MINECRAFT_VERSION}/builds/${BUILD_NUMBER}/downloads/${JAR_NAME}"
 
-cd /data || echo "Error: Failed to change directory to /data" >&2 && exit 1
 
-echo "Downloading ${JAR_NAME} from ${DOWNLOAD_URL} to ${SERVER_BINARY}..."
-rm -f "${SERVER_BINARY}"
-if curl -fsSL -o "${SERVER_BINARY}" "${DOWNLOAD_URL}"; then
-  echo "Download complete: ${SERVER_BINARY}"
-else
-  echo "Error: Download failed for ${JAR_NAME} from ${DOWNLOAD_URL}" >&2
-  rm -f "${SERVER_BINARY}"
-  exit 1
+echo -e "Running curl -o ${SERVER_BINARY} ${DOWNLOAD_URL}"
+
+
+curl -o ${SERVER_BINARY} ${DOWNLOAD_URL}
+
+
+
+if [ ! -f server.properties ]; then
+
+    echo -e "Downloading MC server.properties"
+
+    curl -o server.properties https://raw.githubusercontent.com/parkervcp/eggs/master/minecraft/java/server.properties
+
 fi
